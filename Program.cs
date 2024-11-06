4596f1fb4ffc5398ed46c8ebdb4a34a71c37110f
najiya-n <najiyatsy007@gmail.com>
2024-11-06 10:17:59 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wordcount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("enter a sentence");
+                String sentence = Console.ReadLine();  //take the sentence
+            if (sentence == "")       // sentence should type
+            {
+                Console.WriteLine("should write something");
+
+            }
+            else
+            {
+                string[] words = sentence.Split(' ');  //split sentence
+                int wordcount = words.Length;
+                Console.WriteLine("number of words in the sentence is:" + wordcount);//count the word
+            }
+            Console.ReadLine();
+                 
+            
+            
+        }
+    }
+}
